cc82c187d85ca03cc8c43f1e5e26de036ed6146c
Emil Dimitrov <edimitrov1@gmail.com>
2019-10-27 14:30:40 +0200

    Can add book to library

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+const bookLibrary = [];
+
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+}
+
+function addBookToLibrary(name, author, pages) {
+  const bookToAdd = new Book(name, author, pages);
+  bookLibrary.push(bookToAdd);
 }
