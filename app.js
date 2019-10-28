0724b0020688a47a8475552559156d072779bfe8
Emil Dimitrov <edimitrov1@gmail.com>
2019-10-28 17:37:19 +0200

    Add example games as seed

--- app.js
+++ app.js
@@ -39,6 +39,14 @@ function renderGame(game, on) {
 const gamesUI = document.querySelector('.games');
 const addBookBtn = document.querySelector('.add-game');
 
-addBookBtn.addEventListener('click', handleAddGame);
+// addBookBtn.addEventListener('click', handleAddGame);
+
+addGameToLibrary('GTA V', 'Action', 29.99, 'The game is played from either a third-person or first-person perspective, and its world is navigated on foot or by vehicle. Players control the three lead protagonists throughout single-player and switch between them both during and outside missions.');
+
+addGameToLibrary('Tom Clancy\'s Rainbow Six Siege', 'Shooter', 49.99, 'Each player assumes control of an attacker or a defender in different gameplay modes such as rescuing a hostage, defusing a bomb, and taking control of a capture point. The title has no campaign but features a series of short missions that can be played solo.');
+
+addGameToLibrary('eFootball Pro Evolution Soccer 2020', 'Sports', 69.99,'The new game features a name change with the addition of \'eFootball\' within the title, symbolizing a push in the online gaming space with a focus on PESLeague and eFootball Pro tournaments.');
+
+addGameToLibrary('Rise of Industry', 'Tycoon', 17.99, 'The game sees players build and manage their industrial empire as they attempt to grow and expand in the early 20th century.');
 
 render();
